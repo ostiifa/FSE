ad875107c5e0505e4258084efe65b6ae5d8cf5b4
Оксана Нестеренко <nesterenko_ov_25@student.grsu.by>
2025-10-07 12:24:50 +0300

    Lab_3

---
+++ Lab_3/A1.py
@@ -0,0 +1,10 @@
+x=int(input("Введите координату х "))
+y=int(input("Введите координату y "))
+if (x>0 and y>0):
+    print("I четверть")
+if (x>0 and y<0):
+    print("IV четверть")
+if (x<0 and y>0):
+    print("II четверть")
+if (x<0 and y<0):
+    print("III четверть")
