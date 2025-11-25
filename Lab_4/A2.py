eb79d7970f8dcb9513523fd60929c4d0456e5153
Оксана Нестеренко <nesterenko_ov_25@student.grsu.by>
2025-11-25 10:57:51 +0300

    a2 done

--- Lab_4/A2.py
+++ Lab_4/A2.py
@@ -1,45 +1,41 @@
-def draw_rectangle(n, m, ch="#"):
-    """Рисует прямоугольник размером n x m"""
-    print(f"\nПРЯМОУГОЛЬНИК {n}x{m}:")
-    for i in range(n):
-        for j in range(m):
-            print(ch, end="")
+def draw_rectangle(rows, columns, ch):
+    """Функция для рисования прямоугольника"""
+    print(f"\nПРЯМОУГОЛЬНИК {rows}x{columns}:")
+    for i in range(rows):
+        for j in range(columns):
+            print(ch, end='')
         print()
 
-
-def draw_right_triangle(n, ch="#"):
-    """Рисует правый треугольник из n строк"""
-    print(f"\nПРАВЫЙ ТРЕУГОЛЬНИК ({n} строк):")
-    for i in range(1, n + 1):
-        for j in range(i):
-            print(ch, end="")
-        print()
-
-
-def draw_frame(n, m, ch="#"):
-    """Рисует рамку размером n x m"""
-    print(f"\nРАМКА {n}x{m}:")
-    for i in range(n):
-        for j in range(m):
-            if i == 0 or i == n - 1 or j == 0 or j == m - 1:
-                print(ch, end="")
+def draw_frame(rows, columns, ch):
+    """Функция для рисования рамки"""
+    print(f"\nРАМКА {rows}x{columns}:")
+    for i in range(rows):
+        for j in range(columns):
+            if i == 0 or i == rows - 1 or j == 0 or j == columns - 1:
+                print(ch, end='')
             else:
-                print(" ", end="")
+                print(' ', end='')
         print()
 
-def main():
-    try:
-        n = int(input("Введите количество строк (n): "))
-        m = int(input("Введите количество столбцов (m): "))
+def draw_right_triangle(rows, ch):
+    """Функция для рисования правого треугольника"""
+    print(f"\nПРАВЫЙ ТРЕУГОЛЬНИК ({rows} строк):")
+    for i in range(rows):
+        for j in range(i + 1):
+            print(ch, end='')
+        print()
+print("РИСОВАНИЕ ФИГУР")
+print("=" * 30)
 
-        if n <= 0 or m <= 0:
-            print("Размеры должны быть положительными числами!")
-            return
-        draw_rectangle(n, m)
-        draw_right_triangle(n)
-        draw_frame(n, m)
+try:
+    n_rect = int(input("Введите количество строк для прямоугольника и рамки: "))
+    m_rect = int(input("Введите количество столбцов для прямоугольника и рамки: "))
+    n_triangle = int(input("Введите количество строк для треугольника: "))
+    symbol = input("Введите символ для рисования (по умолчанию #): ") or '#'
+except ValueError:
+    print("Пожалуйста, введите целые числа!")
+    exit()
 
-    except ValueError:
-        print("Пожалуйста, введите целые числа!")
-if __name__ == "__main__":
-    main()
+draw_rectangle(n_rect, m_rect, symbol)
+draw_frame(n_rect, m_rect, symbol)
+draw_right_triangle(n_triangle, symbol)
